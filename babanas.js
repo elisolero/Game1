ff967ac31407172c45118846fd8fc876fd0c25a4
elisolero <eli.solero.web@gmail.com>
2019-08-21 15:33:33 +0300

    add pipe

--- babanas.js
+++ babanas.js
@@ -1,10 +1,18 @@
 function startGame(){
 
+    var checker = false;
+    var topPipe;
+    var bottomPipe;
     var mousePos;
+    
     onmousemove = function(e){
         // console.log("mouse location:", e.clientX, e.clientY)
         mousePos = e;
+        if(bottomPipe){
+            bottomPipe.x = mousePos.x - 80;
+        }
     }
+
     var scoreCounter = 0;
     var score = document.getElementById("score");
     score.innerText = scoreCounter;
@@ -15,12 +23,13 @@ function startGame(){
     // stage.setClearColor("#ccc");
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     // createjs.Ticker.interval = 25;
-
-    createjs.Touch.enable(stage);
+    createjs.Touch.enable(stage, false, true);
+    // debugger;
     var cont = stage.addChild(new createjs.Container())
     .set({mouseEnabled:false, mouseChildren:false});
 
-    addGate();
+    addBottomGate();
+    addTopGate();
 
     // Load the banana
     var img = document.createElement("img");
@@ -126,9 +135,12 @@ function startGame(){
         var max = 5;
         var min = 4;
         var speed = Math.random() * (max - min + 1) + min;
+        var xPlace = (mousePos) ? mousePos.x :  canvas.width/2;
+        // console.log();
+        // debugger;
         b.set({
-            x: mousePos.x,//Math.random() * canvas.width/2,
-            y: canvas.height - 100,
+            x: xPlace,
+            y: bottomPipe.y + bottomPipe.image.height * 0.1,
             speed: speed, //Math.random() * 8 + 1.5,
             rotation: Math.random() * 360,
             rotationSpeed: Math.random() * 3 - 1.5,
@@ -146,9 +158,7 @@ function startGame(){
         // Speed is a factor of scale
     }
 
-   
-
-    function addGate(){
+    function addBottomGateRectangle(){
         var r = 64;
         var graphics = new createjs.Graphics();
         graphics.beginFill("#FF0099")
@@ -161,9 +171,58 @@ function startGame(){
         cached.y = canvas.height / 2;
         cached.cache(-r,-r, r*2,r*2);        
     }
+    
+    function addBottomGate(){
+        var img = document.createElement("img");
+        // img.crossOrigin = "Anonymous";
+        img.src = "./pipeline.svg";
+        img.onload = init;
+
+        let PRECENT = window.innerWidth * 0.3;
+        PRECENT = (PRECENT < 250) ? PRECENT : 250;
+        img.width = PRECENT
+        img.height = PRECENT * 2
+
+        var bmp = new createjs.Bitmap(img);
+        var xPlace = (mousePos) ? mousePos.x :  window.innerWidth/2 - bmp.image.width/2
+
+        cont.addChild(bmp);
+        bmp.set({
+            x: xPlace,
+            y: window.innerHeight  - bmp.image.height * 0.41,
+            rotation:10,
+        });   
+        bottomPipe =  bmp;  
+    }
+
+    function addTopGate(){
+        var img = document.createElement("img");
+        // img.crossOrigin = "Anonymous";
+        img.src = "./pipeline.svg";
+        img.onload = init;
+
+        let PRECENT = window.innerWidth * 0.3;
+        PRECENT = (PRECENT < 250) ? PRECENT : 250;
+        img.width = PRECENT
+        img.height = PRECENT * 2
+
+        var bmp = new createjs.Bitmap(img);
+        // var xPlace = (mousePos) ? mousePos.x :  window.innerWidth/2 - bmp.image.width/2
+
+        cont.addChild(bmp);
+        bmp.set({
+            x: window.innerWidth/2 + (img.width/2),
+            y:  bmp.image.height * 0.41,
+            rotation:200,
+        });   
+        topPipe =  bmp;         
+
+    }
+
     // Resize Code
     window.addEventListener("resize", handleResize, false);
     function handleResize(event) {
+        // debugger;
     var w = window.innerWidth,
         h = window.innerHeight;
     canvas.width = w; canvas.height = h;
